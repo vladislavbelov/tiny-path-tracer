f61d5bc6658a4741f34f4cfb95f0bf1ab6c1dfec
Vladislav Belov <vladislavbelovdev@gmail.com>
2019-11-29 23:05:04 +0300

    Adds linear color output

--- tiny-path-tracer.cpp
+++ tiny-path-tracer.cpp
@@ -1,5 +1,21 @@
 #include <cstdlib>
 #include <fstream>
+#include <vector>
+
+struct Vector3 {
+    float x, y, z; // Coordinates in R3.
+};
+
+template<typename T>
+T clamp(const T& val, const T& min, const T& max) {
+    return std::min(max, std::max(min, val));
+}
+
+void OutputLinearColor(std::ostream& out, const Vector3& v) {
+    out << clamp<int>(v.x * 255, 0, 255) << " "
+        << clamp<int>(v.y * 255, 0, 255) << " "
+        << clamp<int>(v.z * 255, 0, 255) << "\n";
+}
 
 int main(int argc, char* argv[]) {
     int width = 640, height = 480;
@@ -10,7 +26,7 @@ int main(int argc, char* argv[]) {
     out << "P3\n" << width << " " << height << "\n" << 255 << "\n";
     for (int y = 0; y < height; ++y)
         for (int x = 0; x < width; ++x)
-            out << 255 << " " << 255 << " " << 255 << "\n";
+            OutputLinearColor(out, {1.0, 1.0, 1.0});
     return EXIT_SUCCESS;
 }
 
