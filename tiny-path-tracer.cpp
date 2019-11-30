9229d8d17cb3105cd7c7760ea405c5e6cd6c98ae
Vladislav Belov <vladislavbelovdev@gmail.com>
2019-11-30 23:13:41 +0300

    Adds basic Vector3 math

--- tiny-path-tracer.cpp
+++ tiny-path-tracer.cpp
@@ -1,14 +1,48 @@
+#include <cmath>
 #include <cstdlib>
 #include <fstream>
 #include <vector>
+
+template<typename T>
+T clamp(const T& val, const T& min, const T& max) {
+    return std::min(max, std::max(min, val));
+}
 
 struct Vector3 {
     float x, y, z; // Coordinates in R3.
 };
 
-template<typename T>
-T clamp(const T& val, const T& min, const T& max) {
-    return std::min(max, std::max(min, val));
+struct Ray3 {
+    Vector3 o, d; // Origin and direction respectively.
+};
+
+Vector3 operator+(const Vector3& v1, const Vector3& v2) {
+    return {v1.x + v2.x, v1.y + v2.y, v1.z + v2.z};
+}
+Vector3 operator-(const Vector3& v1, const Vector3& v2) {
+    return {v1.x - v2.x, v1.y - v2.y, v1.z - v2.z};
+}
+Vector3 operator-(const Vector3& v) {
+    return {-v.x, -v.y, -v.z};
+}
+Vector3 operator*(const Vector3& v, const float k) {
+    return {v.x * k, v.y * k, v.z * k};
+}
+Vector3 operator/(const Vector3& v, const float k) {
+    return {v.x / k, v.y / k, v.z / k};
+}
+Vector3 operator*(const Vector3& v1, const Vector3& v2) {
+    return {v1.x * v2.x, v1.y * v2.y, v1.z * v2.z};
+}
+float dot(const Vector3& v1, const Vector3& v2) {
+    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+float length(const Vector3& v) {
+    return std::sqrt(dot(v, v));
+}
+Vector3 normalize(const Vector3& v) {
+    const float len = length(v);
+    return len > 0.0 ? v / len : v;
 }
 
 void OutputLinearColor(std::ostream& out, const Vector3& v) {
