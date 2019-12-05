3345373c656fc53f8092042b846772aebeff5afe
Vladislav Belov <vladislavbelovdev@gmail.com>
2019-12-05 23:18:17 +0300

    Adds basic camera

--- tiny-path-tracer.cpp
+++ tiny-path-tracer.cpp
@@ -45,6 +45,10 @@ Vector3 normalize(const Vector3& v) {
     return len > 0.0 ? v / len : v;
 }
 
+Vector3 Trace(const Ray3& ray, int depth = 0) {
+    return (ray.d + Vector3{1, 1, 1}) / 2;
+}
+
 void OutputLinearColor(std::ostream& out, const Vector3& v) {
     out << clamp<int>(v.x * 255, 0, 255) << " "
         << clamp<int>(v.y * 255, 0, 255) << " "
@@ -53,14 +57,19 @@ void OutputLinearColor(std::ostream& out, const Vector3& v) {
 
 int main(int argc, char* argv[]) {
     int width = 640, height = 480;
+    const float aspect_ratio = 1.0 * width / height;
 
     std::ofstream out("result.ppm");
     if (!out)
         return EXIT_FAILURE;
     out << "P3\n" << width << " " << height << "\n" << 255 << "\n";
     for (int y = 0; y < height; ++y)
-        for (int x = 0; x < width; ++x)
-            OutputLinearColor(out, {1.0, 1.0, 1.0});
+        for (int x = 0; x < width; ++x) {
+            const float u = 1.0 * x / width - 0.5;
+            const float v = (1.0 * y / height - 0.5) / aspect_ratio;
+            Ray3 ray{Vector3{0, 0, -1}, Vector3{u, v, 0}};
+            OutputLinearColor(out, Trace(ray));
+        }
     return EXIT_SUCCESS;
 }
 
