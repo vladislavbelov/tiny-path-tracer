86c82d8dff7b45df950657fa618323cd9423c015
Vladislav Belov <vladislavbelovdev@gmail.com>
2019-12-07 02:49:17 +0300

    Adds ray-sphere intersection

--- tiny-path-tracer.cpp
+++ tiny-path-tracer.cpp
@@ -47,15 +47,30 @@ float length(const Vector3& v) {
 }
 Vector3 normalize(const Vector3& v) {
     const float len = length(v);
-    return len > 0.0 ? v / len : v;
+    return len > 0.0f ? v / len : v;
 }
 
 bool Intersect(const Ray3& ray, const Sphere& sphere, Vector3& x, Vector3& normal) {
-    return false;
+    const Vector3 k = ray.o - sphere.p;
+    const float b = dot(k, ray.d);
+    const float c = dot(k, k) - sphere.r * sphere.r;
+    if (b > 0.0f && c > 0.0f)
+        return false; // The origin is outside of the sphere and pointing away.
+    const float d = b * b - c;
+    if (d < 0.0f)
+        return false; // The ray doesn't intersect the sphere.
+    const float ds = std::sqrt(d);
+    const float t0 = -b - ds;
+    const float t1 = -b + ds;
+    x = ray.o + ray.d * (t0 < 0.0f ? t1 : t0);
+    normal = normalize(x - sphere.p);
+    return true;
 }
 
 std::vector<Sphere> spheres = {
-    {{0, 0, -10}, 3}
+    {{0, 0, 10}, 3},
+    {{-2, 1, 8}, 2},
+    {{-2, -1, 12}, 2}
 };
 
 bool FindIntersection(const Ray3& ray, Vector3& x, Vector3& normal) {
@@ -73,8 +88,8 @@ bool FindIntersection(const Ray3& ray, Vector3& x, Vector3& normal) {
     return found;
 }
 
-Vector3 Trace(const Ray3& ray, int depth = 0) {
-    Vector3 x, normal;
+Vector3 Trace(const Ray3& ray) {
+    Vector3 x = ray.o + ray.d * 1e7f, normal;
     if (!FindIntersection(ray, x, normal))
         return Vector3{0, 0, 0};
     return normal;
@@ -88,7 +103,7 @@ void OutputColor(std::ostream& out, const Vector3& v) {
 
 int main(int argc, char* argv[]) {
     int width = 640, height = 480;
-    const float aspect_ratio = 1.0 * width / height;
+    const float aspect_ratio = 1.0f * width / height;
 
     std::ofstream out("result.ppm");
     if (!out)
@@ -96,9 +111,9 @@ int main(int argc, char* argv[]) {
     out << "P3\n" << width << " " << height << "\n" << 255 << "\n";
     for (int y = 0; y < height; ++y)
         for (int x = 0; x < width; ++x) {
-            const float u = 1.0 * x / width - 0.5;
-            const float v = (1.0 * y / height - 0.5) / aspect_ratio;
-            Ray3 ray{Vector3{0, 0, -1}, Vector3{u, v, 0}};
+            const float u = 1.0f * x / width - 0.5f;
+            const float v = (1.0f * y / height - 0.5f) / aspect_ratio;
+            Ray3 ray{Vector3{0, 0, -1}, normalize(Vector3{u, v, 1})};
             OutputColor(out, (Trace(ray) + Vector3{1, 1, 1}) / 2);
         }
     return EXIT_SUCCESS;
